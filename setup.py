1cb04b8c3f0c062cfdf2c578c9069328054e324a
Crowbar Z <crowbarz@outlook.com>
2020-11-05 11:24:12 +0000

    add setuptools script

---
+++ setup.py
@@ -0,0 +1,27 @@
+"""Pioneer AVR API (async) setup.py."""
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="aiopioneer",
+    version="0.0.1",
+    author="Crowbar Z",
+    author_email="crowbarz@outlook.com",
+    description="Asyncio Python library for controlling a Pioneer AVR via its API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/crowbarz/aiopioneer.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Framework :: AsyncIO",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Home Automation",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires=">=3.6",
+)
