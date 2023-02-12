59ad0d9be34888e6f2cc42a8aea9cf6ba8f4b68e
Crowbar Z <crowbarz@outlook.com>
2023-02-12 13:26:30 +0000

    Lock updates when building source dict

--- aiopioneer/pioneer_avr.py
+++ aiopioneer/pioneer_avr.py
@@ -1404,22 +1404,23 @@ class PioneerAVR:
         self._source_id_to_name = {}
         _LOGGER.info("querying AVR source names")
         max_source_id = self._params[PARAM_MAX_SOURCE_ID]
-        for src_id in range(max_source_id + 1):
-            response = await self.send_raw_request(
-                "?RGB" + str(src_id).zfill(2),
-                "RGB",
-                ignore_error=True,
-                rate_limit=False,
-            )
-            if response is None:
-                timeouts += 1
-                _LOGGER.debug("timeout %d retrieving source %s", timeouts, src_id)
-            elif response is not False:
-                timeouts = 0
-                source_name = response[6:]
-                source_number = str(src_id).zfill(2)
-                self._source_name_to_id[source_name] = source_number
-                self._source_id_to_name[source_number] = source_name
+        async with self._update_lock:
+            for src_id in range(max_source_id + 1):
+                response = await self.send_raw_request(
+                    "?RGB" + str(src_id).zfill(2),
+                    "RGB",
+                    ignore_error=True,
+                    rate_limit=False,
+                )
+                if response is None:
+                    timeouts += 1
+                    _LOGGER.debug("timeout %d retrieving source %s", timeouts, src_id)
+                elif response is not False:
+                    timeouts = 0
+                    source_name = response[6:]
+                    source_number = str(src_id).zfill(2)
+                    self._source_name_to_id[source_name] = source_number
+                    self._source_id_to_name[source_number] = source_name
         _LOGGER.debug("source name->id: %s", self._source_name_to_id)
         _LOGGER.debug("source id->name: %s", self._source_id_to_name)
         if not self._source_name_to_id:
@@ -2707,7 +2708,7 @@ class PioneerAVR:
         if debug_updater:
             _LOGGER.debug(">> PioneerAVR._updater_update() started")
         if self._update_lock.locked():
-            _LOGGER.debug("AVR update already running, skipping")
+            _LOGGER.debug("AVR updates locked, skipping")
             return False
         if not self.available:
             _LOGGER.debug("AVR not connected, skipping update")
