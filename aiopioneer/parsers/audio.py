9c04328548287eb7688d8e92ada1f3d66f524293
Jordan H <jordan@hrvy.uk>
2023-03-20 11:42:16 +0000

    Update audio parsers

--- aiopioneer/parsers/audio.py
+++ aiopioneer/parsers/audio.py
@@ -1,9 +1,9 @@
 """Pioneer AVR response parsers for audio parameters."""
 
-from aiopioneer.const import LISTENING_MODES, TONE_MODES, TONE_DB_VALUES, CHANNEL_LEVELS_OBJ
+from aiopioneer.const import LISTENING_MODES, TONE_MODES, TONE_DB_VALUES
 from .const import Response
 
-def sr(raw: str, _param: dict) -> list:
+def listening_mode(raw: str, _param: dict) -> list:
     """Defines a listening mode response parser for Zone 1 returning string values"""
     parsed = []
     parsed.append(Response(raw=raw,
@@ -15,85 +15,53 @@ def sr(raw: str, _param: dict) -> list:
                          queue_commands=None))
     return parsed
 
-def to(raw: str, _param: dict) -> list:
+def tone(raw: str, _param: dict, zone = "1", command = "TO") -> list:
     """Defines a tone mode response parser for Zone 1 returning string values"""
     parsed = []
     parsed.append(Response(raw=raw,
-                         response_command="to",
+                         response_command=command,
                          base_property="tone",
                          property_name="status",
-                         zone="1",
+                         zone=zone,
                          value=TONE_MODES.get(raw),
                          queue_commands=None))
     return parsed
 
-def ba(raw: str, _param: dict) -> list:
+def tone_bass(raw: str, _param: dict, zone = "1", command = "BA") -> list:
     """Defines a tone bass response parser for Zone 1 returning string values"""
     parsed = []
     parsed.append(Response(raw=raw,
-                         response_command="ba",
+                         response_command=command,
                          base_property="tone",
                          property_name="bass",
-                         zone="1",
+                         zone=zone,
                          value=TONE_DB_VALUES.get(raw),
                          queue_commands=None))
     return parsed
 
-def tr(raw: str, _param: dict) -> list:
+def tone_treble(raw: str, _param: dict, zone = "1", command = "TO") -> list:
     """Defines a tone treble response parser for Zone 1 returning string values"""
     parsed = []
     parsed.append(Response(raw=raw,
-                         response_command="tr",
+                         response_command=command,
                          base_property="tone",
                          property_name="treble",
-                         zone="1",
+                         zone=zone,
                          value=TONE_DB_VALUES.get(raw),
                          queue_commands=None))
     return parsed
 
-def zga(raw: str, _param: dict) -> list:
-    """Defines a tone mode response parser for Zone 2 returning string values"""
-    # Reuse the response parser from Zone 1 but override the zone before returning
-    parsed = to(raw, _param)
-    parsed[0].zone="2"
-    return parsed
-
-def zgb(raw: str, _param: dict) -> list:
-    """Defines a tone bass response parser for Zone 2 returning string values"""
-    # Reuse the response parser from Zone 1 but override the zone before returning
-    parsed = ba(raw, _param)
-    parsed[0].zone="2"
-    return parsed
-
-def zgc(raw: str, _param: dict) -> list:
-    """Defines a tone treble response parser for Zone 2 returning string values"""
-    # Reuse the response parser from Zone 1 but override the zone before returning
-    parsed = tr(raw, _param)
-    parsed[0].zone="2"
-    return parsed
-
-def clv(raw: str, _param: dict) -> list:
+def channel_levels(raw: str, _param: dict, zone = "1", command = "CLV") -> list:
+    """Defines a channel levels resposne object for Zone 1 returning float values"""
     value = float(int(raw[6:]) - 50) / 2
     speaker = str(raw[3:6]).strip("_").upper()
 
     parsed = []
     parsed.append(Response(raw=raw,
-                         response_command="clv",
+                         response_command=command,
                          base_property="channel_levels",
                          property_name=speaker,
-                         zone="1",
+                         zone=zone,
                          value=value,
                          queue_commands=None))
-    return parsed
-
-def zge(raw: str, _param: dict) -> list:
-    parsed = clv(raw, _param)
-    parsed[0].zone = "2"
-    parsed[0].response_command = "zge"
-    return parsed
-
-def zhe(raw: str, _param: dict) -> list:
-    parsed = clv(raw, _param)
-    parsed[0].zone = "3"
-    parsed[0].response_command = "zhe"
     return parsed
