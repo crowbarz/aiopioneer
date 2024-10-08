8d4d8a7caed3ab3cc4e64a79edee085d37196b40
glassbase <glasscorey@gmail.com>
2024-10-07 20:20:45 -0400

    Add Dolby Surround (0040) to model parameters for VSX-1130

--- aiopioneer/param.py
+++ aiopioneer/param.py
@@ -363,6 +363,14 @@ PARAM_MODEL_DEFAULTS = OrderedDict(
             },
         ),
         (
+            r"^VSX-1130",
+            {
+                PARAM_EXTRA_LISTENING_MODES: {
+                    "0040": ["Dolby Surround", True, True],
+                },
+            },
+        ),
+        (
             r"^VSX-S510",
             {
                 PARAM_VOLUME_STEP_ONLY: True,
