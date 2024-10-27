21a98381fdb5147f22076c965fbe0aa8ef08cbce
glassbase <glasscorey@gmail.com>
2024-10-27 13:37:26 -0400

    Add extended stereo 0041 and advanced stereo (cyclic) 0100

--- aiopioneer/param.py
+++ aiopioneer/param.py
@@ -367,6 +367,8 @@ PARAM_MODEL_DEFAULTS = OrderedDict(
             {
                 PARAM_EXTRA_LISTENING_MODES: {
                     "0040": ["Dolby Surround", True, True],
+                    "0041": ["EXTENDED STEREO", True, True],
+                    "0100": ["ADVANCED SURROUND (cyclic)", True, True],
                 },
             },
         ),
